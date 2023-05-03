7f6623a3969631f5fdc684fb2959b25f15cac50c
diefpt <diefpt@gmail.com>
2023-05-03 02:26:10 -0600

    adición del cambio en los elementos desplegados al clickear botones con o sin texto

--- Encriptador/scripts.js
+++ Encriptador/scripts.js
@@ -3,18 +3,42 @@ const encriptar = document.querySelector('#encriptar');
 const desencriptar = document.querySelector('#desencriptar');
 const resultado = document.querySelector('#parrafo-display');
 
+const errorImg = document.querySelector('.no-mensaje-img');
+const errorMsj = document.querySelector('.no-mensaje');
+
 const CLAVE = {a: 'ai', e: 'enter', i: 'imes', o: 'ober', u: 'ufat',};
 
 const cipher = msj => {
-    resultado.innerHTML = msj.replace(/[a-z]/g, char => CLAVE.hasOwnProperty(char) ? CLAVE[char] : char);
+    if(!msj) {
+        errorImg.style.visibility = 'visible';
+        errorMsj.style.visibility = 'visible';
+        resultado.style.top = '20%';
+    } else {
+        errorImg.style.visibility = 'hidden';
+        errorMsj.style.visibility = 'hidden';
+        resultado.style.top = '-45%';
+    }
+
+    resultado.innerHTML = msj ? msj.replace(/[a-z]/g, char => CLAVE.hasOwnProperty(char) ? CLAVE[char] : char) : 'Ingresa el texto que desees encriptar o desencriptar';
 }    
 
 const deCipher = msj => {
-    resultado.innerHTML = msj.replaceAll('ai', 'a')
+    if(!msj) {
+        errorImg.style.visibility = 'visible';
+        errorMsj.style.visibility = 'visible';
+        resultado.style.top = '20%';
+    } else {
+        errorImg.style.visibility = 'hidden';
+        errorMsj.style.visibility = 'hidden';
+        resultado.style.top = '-45%';
+    }
+
+    resultado.innerHTML = msj ?  msj.replaceAll('ai', 'a')
                              .replaceAll('enter', 'e')
                              .replaceAll('imes', 'i')
                              .replaceAll('ober', 'o')
-                             .replaceAll('ufat', 'u');
+                             .replaceAll('ufat', 'u') :
+    'Ingresa el texto que desees encriptar o desencriptar';
 }
 
 encriptar.addEventListener('click', () => {
