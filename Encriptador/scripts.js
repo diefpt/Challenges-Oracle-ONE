838053e18c6f535efafe98f9f3f74ffc242e17d5
diefpt <diefpt@gmail.com>
2023-05-04 20:36:32 -0600

    renombre de variables, constantes, clases e ids

--- Encriptador/scripts.js
+++ Encriptador/scripts.js
@@ -1,30 +1,29 @@
-const text = document.querySelector('#text');
-const encriptar = document.querySelector('#encriptar');
-const desencriptar = document.querySelector('#desencriptar');
-const resultado = document.querySelector('#parrafo-display');
-const copy = document.querySelector('#copiar');
-const CLAVE = {a: 'ai', e: 'enter', i: 'imes', o: 'ober', u: 'ufat',};
+const textInput = document.querySelector('#input');
+const textDisplay = document.querySelector('#display');
+
+const buttonCipher = document.querySelector('#cipher');
+const buttonDecipher = document.querySelector('#decipher');
+const buttonCopy = document.querySelector('#copy');
+
+const KEY = {a: 'ai', e: 'enter', i: 'imes', o: 'ober', u: 'ufat',};
 const errorMsj = '<br><br><br><br><br><a href="https://shorturl.at/mvY09"><img src="imagenes/5203299.jpg" class="no-mensaje-img" width="250px" alt="Imagen de storyset en Freepik"></a><h3>Ningún mensaje fue encontrado</h3><p>Ingresa el texto que desees encriptar o desencriptar</p>'
 
-const cipher = msj => {
-    copy.style.visibility = msj ? 'visible' : 'hidden';
-    resultado.innerHTML = msj ? 
-    msj.replace(/[a-z]/gi, char => CLAVE.hasOwnProperty(char) ? CLAVE[char] : char) : 
-    errorMsj;
-}    
-
-const deCipher = msj => {
-    copy.style.visibility = msj ? 'visible' : 'hidden';
-    resultado.innerHTML = msj ?  msj.replaceAll('ai', 'a')
-                             .replaceAll('enter', 'e')
-                             .replaceAll('imes', 'i')
-                             .replaceAll('ober', 'o')
-                             .replaceAll('ufat', 'u') :
-    errorMsj;
+cipher = text => {
+    buttonCopy.style.visibility = text ? 'visible' : 'hidden';
+    textDisplay.innerHTML = text ? text.replace(/[a-z]/gi, char => KEY.hasOwnProperty(char) ? KEY[char] : char) : errorMsj;
 }
 
-encriptar.addEventListener('click', () => {cipher(text.value);});
-desencriptar.addEventListener('click', () => {deCipher(text.value);});
-copy.addEventListener('click', () => {
-    navigator.clipboard.writeText(resultado.innerHTML);
+decipher = text => {
+    buttonCopy.style.visibility = text ? 'visible' : 'hidden';
+    textDisplay.innerHTML = text ?  text.replaceAll('ai', 'a')
+                            .replaceAll('enter', 'e')
+                            .replaceAll('imes', 'i')
+                            .replaceAll('ober', 'o')
+                            .replaceAll('ufat', 'u') : errorMsj;
+}
+
+buttonCipher.addEventListener('click', () => {cipher(textInput.value);});
+buttonDecipher.addEventListener('click', () => {decipher(textInput.value);});
+buttonCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(textDisplay.innerHTML);
 });
