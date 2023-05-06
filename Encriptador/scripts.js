9fea352ffd2e5a07036763c90a2aa8cb9cb95c89
diefpt <diefpt@gmail.com>
2023-05-06 00:35:48 -0600

    implementación de la pantalla responsiva

--- Encriptador/scripts.js
+++ Encriptador/scripts.js
@@ -6,15 +6,26 @@ const buttonDecipher = document.querySelector('#decipher');
 const buttonCopy = document.querySelector('#copy');
 
 const KEY = {a: 'ai', e: 'enter', i: 'imes', o: 'ober', u: 'ufat',};
-const errorMsj = '<br><br><br><br><br><img src="imagenes/muñeco.png" class="no-mensaje-img" width="250px"><h3>Ningún mensaje fue encontrado</h3><p>Ingresa el texto que desees encriptar o desencriptar</p>'
+let errorMsj = '<h3>Ningún mensaje fue encontrado</h3><p>Ingresa el texto que desees encriptar o desencriptar</p>';
+
+const windowWidth = screen.width;
+const img = document.querySelector('.no-message-img');
+const displayDiv = document.querySelector('.display');
+if(windowWidth <= 850) {
+    img.remove();
+} else {
+    errorMsj = '<img src="imagenes/muñeco.png" class="no-mensaje-img" width="250px">' + errorMsj;
+}
 
 cipher = text => {
     buttonCopy.style.visibility = text ? 'visible' : 'hidden';
+    displayDiv.style.padding = text || windowWidth <= 850 ? '0px 8px' : '100px 8px 0px 8px';
     textDisplay.innerHTML = text ? text.replace(/[a-z]/gi, char => KEY.hasOwnProperty(char) ? KEY[char] : char) : errorMsj;
 }
 
 decipher = text => {
     buttonCopy.style.visibility = text ? 'visible' : 'hidden';
+    displayDiv.style.padding = text || windowWidth <= 850 ? '0px 8px' : '100px 8px 0px 8px';
     textDisplay.innerHTML = text ?  text.replaceAll('ai', 'a')
                             .replaceAll('enter', 'e')
                             .replaceAll('imes', 'i')
